08fa5f23054869faa8d40cf2ade365bf14832386
juggl3r1337 <juggl3r1337@proton.me>
2026-01-27 20:46:15 -0500

    fixed a couple things

--- scripts/install.sh
+++ scripts/install.sh
@@ -28,14 +28,14 @@ git clone https://github.com/alacritty/alacritty.git
 cd alacritty
 rustup override set stable
 rustup update stable
-apt install -y cmake g++ pkg-config libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 gzip scdoc
+sudo apt install -y cmake g++ pkg-config libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 gzip scdoc
 cargo build --release
 sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
 sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 sudo desktop-file-install extra/linux/Alacritty.desktop
 sudo update-desktop-database
-mkdir -p /usr/local/share/man/man1
-mkdir -p /usr/local/share/man/man5
+sudo mkdir -p /usr/local/share/man/man1
+sudo mkdir -p /usr/local/share/man/man5
 scdoc < extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
 scdoc < extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
 scdoc < extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz > /dev/null
--- scripts/prepareTools.sh
+++ scripts/prepareTools.sh
@@ -26,7 +26,7 @@ curl https://sliver.sh/install|sudo bash
 ## Install golang
 echo '[+] Installing golang'
 wget https://go.dev/dl/go1.24.3.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.3.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.24.3.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile
 rm go1.24.3.linux-amd64.tar.gz
 
